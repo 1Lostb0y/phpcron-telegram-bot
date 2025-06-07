927ad9e98ab36aee2a8a9da9e4fa1a821ef548f3
1Lostb0y <amin_azmoodh@outlook.com>
2025-06-07 10:06:07 +0400

    Add Composer dependencies and autoloading configuration
    - Created .vscode/settings.json to disable dockerrc.
    - Added composer.lock to lock project dependencies.
    - Generated vendor/autoload.php for autoloading classes.
    - Implemented Composer ClassLoader in vendor/composer/ClassLoader.php.
    - Added InstalledVersions.php to track installed packages.
    - Included LICENSE file for Composer.
    - Generated autoload_classmap.php for class mapping.
    - Created autoload_namespaces.php and autoload_psr4.php for namespace autoloading.
    - Generated autoload_real.php and autoload_static.php for autoloading functionality.
    - Updated installed.json and installed.php to reflect current package state.

---
+++ telegram-bot/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'phpcron/telegram-bot',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '94777ed93eb48ce03bb47a73b8601a9234c2127e',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpcron/telegram-bot' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '94777ed93eb48ce03bb47a73b8601a9234c2127e',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
